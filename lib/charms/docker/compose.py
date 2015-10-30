139ae975082bb1281d3115e8ddf1072192744145
Charles Butler <chuck@dasroot.net>
2015-10-30 17:44:06 -0400

    Raise error on init if the workspace is not a valid compose workspace

--- lib/charms/docker/compose.py
+++ lib/charms/docker/compose.py
@@ -8,6 +8,7 @@ from subprocess import check_call
 class Compose:
     def __init__(self, workspace):
         self.workspace = workspace
+        self.validate_workspace()
 
     def start_service(self, service=None):
         if service:
@@ -27,14 +28,18 @@ class Compose:
         with chdir(self.workspace):
             check_call(split(cmd))
 
+
+    def validate_workspace(self):
+        dcyml = os.path.isfile("{}/docker-compose.yml".format(self.workspace))
+        dcyaml = os.path.isfile("{}/docker-compose.yaml".format(self.workspace))
+        if not dcyml and not dcyaml:
+            raise OSError("docker-compose.yml not found in workspace")
+
 # This is helpful for setting working directory context
 @contextmanager
 def chdir(path):
     '''Change the current working directory to a different directory to run
     commands and return to the previous directory after the command is done.'''
-    if not os.path.exists("{}/docker-compose.yml".format(self.workspace) or
-    not os.path.exists("{}/docker-compose.yaml".format(self.workspace))):
-        raise OSError "docker-compose.yml not found in workspace"
     old_dir = os.getcwd()
     os.chdir(path)
     yield
