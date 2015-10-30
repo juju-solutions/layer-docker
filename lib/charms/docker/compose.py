7e41c1eddf64ec17f186554c92db6a7a24468633
Charles Butler <chuck@dasroot.net>
2015-10-30 17:22:37 -0400

    Updates the lib/charms/docker/compose.py to remove duplication

--- lib/charms/docker/compose.py
+++ lib/charms/docker/compose.py
@@ -10,29 +10,31 @@ class Compose:
         self.workspace = workspace
 
     def start_service(self, service=None):
-        with chdir(self.workspace):
-            if service:
-                cmd = "docker-compose up -d {}".format(service)
-            else:
-                cmd = "docker-compose up -d"
-            self.run(cmd)
+        if service:
+            cmd = "docker-compose up -d {}".format(service)
+        else:
+            cmd = "docker-compose up -d"
+        self.run(cmd)
 
     def kill_service(self, service=None):
-        with chdir(self.workspace):
-            if service:
-                cmd = "docker-compose kill -d {}".format(service)
-            else:
-                cmd = "docker-compose kill"
-            self.run(cmd)
+        if service:
+            cmd = "docker-compose kill -d {}".format(service)
+        else:
+            cmd = "docker-compose kill"
+        self.run(cmd)
 
     def run(self, cmd):
-        check_call(split(cmd))
+        with chdir(self.workspace):
+            check_call(split(cmd))
 
 # This is helpful for setting working directory context
 @contextmanager
 def chdir(path):
     '''Change the current working directory to a different directory to run
     commands and return to the previous directory after the command is done.'''
+    if not os.path.exists("{}/docker-compose.yml".format(self.workspace) or
+    not os.path.exists("{}"/docker-compose.yaml)):
+        raise OSError "docker-compose.yml not found in workspace"
     old_dir = os.getcwd()
     os.chdir(path)
     yield
