e74c886cb9803e274b37382c13de7c7211a61cd7
Charles Butler <chuck@dasroot.net>
2016-04-18 11:04:16 -0400

    Adds a WIP fix for the dockerpy issue
    The core problem was with dockerpy requiring newer setuptools than even our
    charm builder env was using. So that needed to be bumped outside of the primary
    tactics.
    
    There is room for refactoring here to make this much cleaner rather than
    supporting a massive duplicate body of code

---
+++ tactics/docker.py
@@ -0,0 +1,21 @@
+from charmtools.build.tactics import WheelhouseTactic
+
+
+class DockerWheelhouseTactic(WheelhouseTactic):
+    def __call__(self, venv=None):
+        from charmtools import utils
+        create_venv = venv is None
+        venv = venv or path(tempfile.mkdtemp())
+        pip = venv / 'bin' / 'pip3'
+        wheelhouse = self.target.directory / 'wheelhouse'
+        wheelhouse.mkdir_p()
+        if create_venv:
+            import pdb; pdb.set_trace()
+            utils.Process(('virtualenv', '--python', 'python3', venv)).exit_on_error()()
+            utils.Process((pip, 'install', '-U', 'pip', 'wheel')).exit_on_error()()
+        utils.Process((pip, 'install', '-U', 'setuptools')).exit_on_error()()  # custom bit
+        for tactic in self.previous:
+            tactic(venv)
+        self._add(pip, wheelhouse, '-r', self.entity)
+        if create_venv:
+            venv.rmtree_p()
