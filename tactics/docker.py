0586fb0d0b8ebab4100c25958431aded6e86e4f4
Charles Butler <chuck@dasroot.net>
2016-04-18 12:47:49 -0400

    Adds imports to satisfy lint

--- tactics/docker.py
+++ tactics/docker.py
@@ -1,10 +1,16 @@
 from charmtools.build.tactics import WheelhouseTactic
+from charmtools.utils import Process
+import tempfile
+from path import Path as path
 
-# The references to process, path, and tempfile are implicit below because
-# they come from the global scope of charmtools.utils which gets passed in during 
-# charmtools.utils.load_class
-# Investigate there if there is abrupt breakage here.
+
 class DockerWheelhouseTactic(WheelhouseTactic):
+    ''' The references to process, path, and tempfile are implicit
+    below because  they come from the global scope of
+    charmtools.utils which gets passed in during
+    charmtools.utils.load_class
+    Investigate there if there is abrupt breakage here.
+    '''
     def __call__(self, venv=None):
         create_venv = venv is None
         venv = venv or path(tempfile.mkdtemp())
@@ -12,9 +18,9 @@ class DockerWheelhouseTactic(WheelhouseTactic):
         wheelhouse = self.target.directory / 'wheelhouse'
         wheelhouse.mkdir_p()
         if create_venv:
-            Process(('virtualenv', '--python', 'python3', venv)).exit_on_error()()
-            Process((pip, 'install', '-U', 'pip', 'wheel')).exit_on_error()()
-        Process((pip, 'install', '-U', 'setuptools')).exit_on_error()()  # custom bit
+            Process(('virtualenv', '--python', 'python3', venv)).exit_on_error()()  # noqa
+            Process((pip, 'install', '-U', 'pip', 'wheel')).exit_on_error()()  # noqa
+        Process((pip, 'install', '-U', 'setuptools')).exit_on_error()()  # noqa
         for tactic in self.previous:
             tactic(venv)
         self._add(pip, wheelhouse, '-r', self.entity)
