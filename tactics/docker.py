7704babf93b6d78c157a3d3a3dcc2ac349a1104b
Charles Butler <chuck@dasroot.net>
2016-04-18 16:23:21 +0000

    Its blue

--- tactics/docker.py
+++ tactics/docker.py
@@ -1,20 +1,20 @@
 from charmtools.build.tactics import WheelhouseTactic
 
-
+# The references to process, path, and tempfile are implicit below because
+# they come from the global scope of charmtools.utils which gets passed in during 
+# charmtools.utils.load_class
+# Investigate there if there is abrupt breakage here.
 class DockerWheelhouseTactic(WheelhouseTactic):
     def __call__(self, venv=None):
-        # this was due to some weird scoping issue during execution.
-        # investigate more later
-        from charmtools import utils
         create_venv = venv is None
         venv = venv or path(tempfile.mkdtemp())
         pip = venv / 'bin' / 'pip3'
         wheelhouse = self.target.directory / 'wheelhouse'
         wheelhouse.mkdir_p()
         if create_venv:
-            utils.Process(('virtualenv', '--python', 'python3', venv)).exit_on_error()()
-            utils.Process((pip, 'install', '-U', 'pip', 'wheel')).exit_on_error()()
-        utils.Process((pip, 'install', '-U', 'setuptools')).exit_on_error()()  # custom bit
+            Process(('virtualenv', '--python', 'python3', venv)).exit_on_error()()
+            Process((pip, 'install', '-U', 'pip', 'wheel')).exit_on_error()()
+        Process((pip, 'install', '-U', 'setuptools')).exit_on_error()()  # custom bit
         for tactic in self.previous:
             tactic(venv)
         self._add(pip, wheelhouse, '-r', self.entity)
